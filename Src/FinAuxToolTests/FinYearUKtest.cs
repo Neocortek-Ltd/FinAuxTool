713d72b1ab89919e451c7553bad6b2ca12a06cf9
danielgorin <d.gorin82@gmail.com>
2022-12-18 09:39:31 +0000

    Fixed unit test for FinYearUK constructor of composite class.

--- Src/FinAuxToolTests/FinYearUKtest.cs
+++ Src/FinAuxToolTests/FinYearUKtest.cs
@@ -8,29 +8,28 @@ public class FinYearUKtest
     public void FinYearUKconstructorTestvalidYear()
     {
         //Arrange
-        FinYearUK finYearUK;
+        var expectedPeriods = new Dictionary<short, short[]>()
+        {
+            {2, new short[]{4, 5, 6}},
+            {3, new short[]{7, 8, 9}},
+            {4, new short[]{10, 11, 12}},
+            {1, new short[]{1, 2, 3}},
+        };
 
         //Act
-        finYearUK = new FinYearUK(2022);
+        FinYearUK finYearUK = new FinYearUK(2022);;
         var actualPeriods = finYearUK.Quarters
             .ToDictionary(quarter => quarter.PerVal, quarter => quarter.Months.Select(month => month.PerVal)
             .ToArray());
 
-        // var actualPeriods = finYearUK.Quarters
-        //     .Select(quarter => quarter.PerVal).ToArray();
-        //
-        // Assert
-         var expectedPeriods = new Dictionary<short, short[]>()
-         {
-             {2, new short[]{4, 5, 6}},
-             {3, new short[]{7, 8, 9}},
-             {4, new short[]{10, 11, 12}},
-             {1, new short[]{1, 2, 3}},
-         };
-
-        // var expectedPeriods = new short[] { 2, 3, 4, 1 };
-
-        CollectionAssert.AreEqual(expectedPeriods.ToList(), actualPeriods.ToList());
+        // Assert that Quarters are correct
+        CollectionAssert.AreEqual(expectedPeriods.Keys, actualPeriods.Keys);
+        
+        // Assert that Months in each Quarter are correct
+        foreach (var quarter in actualPeriods.Keys)
+        {
+            CollectionAssert.AreEqual(expectedPeriods[quarter],actualPeriods[quarter]);
+        }
     }
 }
 
